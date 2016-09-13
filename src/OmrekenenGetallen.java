3d5bcd3d94a3ab1e072ea4de78c9d785daa006c3
nardswolfs <nard.swolfs@hotmail.com>
2016-09-13 10:00:27 +0200

    first push

---
+++ src/OmrekenenGetallen.java
@@ -0,0 +1,16 @@
+/**
+ * Created by nards on 13-9-2016.
+ */
+public class OmrekenenGetallen {
+    public static void Main(String[] args) {
+
+    }
+
+    public void BerekenGetallen() {
+        KelvinNaarCelcius(254);
+    }
+
+    public double KelvinNaarCelcius(double Kelvin) {
+        return Kelvin - 260;
+    }
+}
